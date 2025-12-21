37b6c910b9565efd7829c2ca85f296e339bc8a37
bala-amypo <bala@amypo.in>
2025-12-21 20:18:07 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/ServiceCounterService.java
@@ -0,0 +1,13 @@
+ServiceCounterService.java
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.entity.ServiceCounter;
+
+public interface ServiceCounterService {
+
+    ServiceCounter addCounter(ServiceCounter counter);
+
+    List<ServiceCounter> getActiveCounters();
+}
