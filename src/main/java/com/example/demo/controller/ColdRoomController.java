9ff864e81bbf84324ccd82e6d25f040b59a9baf5
bala-amypo <bala@amypo.in>
2025-12-21 20:34:19 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/ColdRoomController.java
@@ -0,0 +1,34 @@
+//ColdRoomController.java
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.ColdRoom;
+import com.example.demo.service.ColdRoomService;
+
+@RestController
+public class ColdRoomController {
+
+    private final ColdRoomService coldRoomService;
+
+    public ColdRoomController(ColdRoomService coldRoomService) {
+        this.coldRoomService = coldRoomService;
+    }
+
+    // POST /cold-rooms
+    @PostMapping("/cold-rooms")
+    public ColdRoom createColdRoom(@RequestBody ColdRoom coldRoom) {
+        return coldRoomService.addColdRoom(coldRoom);
+    }
+
+    // GET /cold-rooms
+    @GetMapping("/cold-rooms")
+    public List<ColdRoom> getAllColdRooms() {
+        return coldRoomService.getAllColdRooms();
+    }
+}
