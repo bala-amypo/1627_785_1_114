e072a134a65467e0ed42a2e49b50441ecd821d83
bala-amypo <bala@amypo.in>
2025-12-22 09:45:32 +0000

    User commit

--- src/main/java/com/example/demo/service/ColdRoomService.java
+++ src/main/java/com/example/demo/service/ColdRoomService.java
@@ -1,12 +1,12 @@
-package com.example.demo.service;
+// package com.example.demo.service;
 
-import java.util.List;
+// import java.util.List;
 
-import com.example.demo.entity.ColdRoom;
+// import com.example.demo.entity.ColdRoom;
 
-public interface ColdRoomService {
+// public interface ColdRoomService {
 
-    ColdRoom addColdRoom(ColdRoom coldRoom);
+//     ColdRoom addColdRoom(ColdRoom coldRoom);
 
-    List<ColdRoom> getAllColdRooms();
-}
+//     List<ColdRoom> getAllColdRooms();
+// }
