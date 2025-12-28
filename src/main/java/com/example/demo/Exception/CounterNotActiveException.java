eeb2267670e9150b844c04e38ebead10eb5ca396
bala-amypo <bala@amypo.in>
2025-12-28 17:45:38 +0000

    User commit

--- src/main/java/com/example/demo/Exception/CounterNotActiveException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.exception;
-
-public class CounterNotActiveException extends RuntimeException {
-    public CounterNotActiveException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/demo/Exception/InvalidTokenStatusException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.exception;
-
-public class InvalidTokenStatusException extends RuntimeException {
-    public InvalidTokenStatusException(String message) {
-        super(message);
-    }
-}
